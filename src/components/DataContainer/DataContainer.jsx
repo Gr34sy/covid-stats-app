9c6afdd3e59b2b03a21492e90627cdd167efded3
Gr34sy <slupek3@poczta.fm>
2023-04-07 22:57:18 +0200

    handled api comunication

--- src/components/DataContainer/DataContainer.jsx
+++ src/components/DataContainer/DataContainer.jsx
@@ -1,13 +1,47 @@
 import React, { useEffect, useState} from 'react'
 
-//https://covid19api.com/)
+//https://api.covid19api.com/all)
 
 import './data-container.css';
 
 const DataContainer = () => {
+
+    const [covidCountries, setCovidCountries] = useState([]);
+    const [covidGlobal, setCovidGlobal] = useState([]);
+
+    useEffect(() => {
+        try {
+
+            fetch('https://api.covid19api.com/summary')
+            .then(resp => {
+                return resp.json();
+            })
+            .then(data => {
+                setCovidCountries(data.Countries);
+                setCovidGlobal(data.Global);
+                console.log(data)
+            })
+
+        } catch (err){
+            console.error(err);
+        }
+        
+    }, [])
+
+    const GlobalInfo = () => (
+        <section className='app__global-info'>
+            <h2 className='app__global-info_heading'>Global Stats</h2>
+            <p className='app__global-info_line'> <span>Total Confirmed:</span>{covidGlobal.TotalConfirmed} </p>
+            <p className='app__global-info_line'> <span>Total Deaths:</span>{covidGlobal.TotalDeaths} </p>
+
+            <p className='app__global-info_line'> <span>New Confirmed:</span>{covidGlobal.NewConfirmed} </p>
+            <p className='app__global-info_line'> <span>New Deaths:</span>{covidGlobal.NewDeaths} </p>
+        </section>
+    );
+
   return (
     <section className='app__data-container'>
-
+        <GlobalInfo />
     </section>
   )
 }
