ec3b5aecb02daa374ef0fab208c230526d915b8b
Gr34sy <slupek3@poczta.fm>
2023-04-07 16:07:52 +0200

    added base files to the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Covid Stats</h1>
+        <p>Parafsalfkn sakdmjaksdn</p>
+    </div>
+  );
+}
+
+export default App;
